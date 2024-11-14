5e458e7ed3ccb0a5656f303dd00c7606e8160ba6
AfrinDange <dangeaman2003@gmail.com>
2024-11-14 15:51:09 +0530

    Updated Header.js

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `#8A2BE2`,
+      background: `#6A0DAD`,
       marginBottom: `1.45rem`,
     }}
   >
